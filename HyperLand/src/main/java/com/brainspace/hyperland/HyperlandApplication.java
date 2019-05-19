6908b092793a9f546872d499b2f5b8de1a8bda83
pramodd09 <pankaj.iitr30@gmail.com>
2019-05-19 12:56:17 +0530

    booking id added in booking query

--- HyperLand/src/main/java/com/brainspace/hyperland/HyperlandApplication.java
+++ HyperLand/src/main/java/com/brainspace/hyperland/HyperlandApplication.java
@@ -8,13 +8,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 //@EnableResourceServer
 @SpringBootApplication
-public class HyperlandApplication extends SpringBootServletInitializer {
+public class HyperlandApplication {//extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(HyperlandApplication.class, args);
 	}
-
+/*
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(HyperlandApplication.class);
-	}
+	}*/
 }
